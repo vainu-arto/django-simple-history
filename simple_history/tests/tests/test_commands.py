vainu-arto/django-simple-history

commit 68497f32eac209624389ef60c9fefd71d5d0f29f

Add support for `included_fields` when diffing

simple_history/models.py
@@ -585,7 +585,7 @@ class HistoricalObjectDescriptor:
 
 
 class HistoricalChanges:
-    def diff_against(self, old_history, excluded_fields=None):
+    def diff_against(self, old_history, excluded_fields=None, included_fields=None):
         if not isinstance(old_history, type(self)):
             raise TypeError(
                 ("unsupported type(s) for diffing: " "'{}' and '{}'").format(
@@ -595,13 +595,10 @@ class HistoricalChanges:
         if excluded_fields is None:
             excluded_fields = set()
 
-        excluded_fields = set(excluded_fields).union(self._history_excluded_fields)
+        if included_fields is None:
+            included_fields = {f.name for f in old_history.instance_type._meta.fields}
 
-        fields = {
-            f.name
-            for f in old_history.instance_type._meta.fields
-            if f.name not in excluded_fields
-        }
+        fields = set(included_fields).difference(excluded_fields)
 
         changes = []
         changed_fields = []

simple_history/tests/tests/test_models.py
@@ -668,6 +668,19 @@ class HistoricalRecordsTest(TestCase):
         self.assertEqual(delta.changed_fields, [])
         self.assertEqual(delta.changes, [])
 
+    def test_history_diff_with_included_fields(self):
+        p = Poll.objects.create(question="what's up?", pub_date=today)
+        p.question = "what's up, man?"
+        p.save()
+        new_record, old_record = p.history.all()
+        delta = new_record.diff_against(old_record, included_fields=[])
+        self.assertEqual(delta.changed_fields, [])
+        self.assertEqual(delta.changes, [])
+
+        delta = new_record.diff_against(old_record, included_fields=["question"])
+        self.assertEqual(delta.changed_fields, ["question"])
+        self.assertEqual(len(delta.changes), 1)
+
 
 class GetPrevRecordAndNextRecordTestCase(TestCase):
     def assertRecordsMatch(self, record_a, record_b):

commit 502c0e7715354029128316a90b6ea4c171ad1716

Add test for cleaning history with model level excluded fields
This was broken in commit 68497f32eac2.

simple_history/tests/tests/test_commands.py
@@ -410,6 +410,28 @@ class TestCleanDuplicateHistory(TestCase):
         )
         self.assertEqual(Poll.history.all().count(), 1)
 
+    def test_auto_cleanup_with_model_excluded_fields(self):
+        p = PollWithExcludeFields.objects.create(
+            question="Will this be deleted?", pub_date=datetime.now()
+        )
+        self.assertEqual(PollWithExcludeFields.history.all().count(), 1)
+        p.pub_date = p.pub_date + timedelta(days=1)
+        p.save()
+        self.assertEqual(PollWithExcludeFields.history.all().count(), 2)
+        out = StringIO()
+        management.call_command(
+            self.command_name,
+            auto=True,
+            stdout=out,
+            stderr=StringIO(),
+        )
+        self.assertEqual(
+            out.getvalue(),
+            "Removed 1 historical records for "
+            "<class 'simple_history.tests.models.PollWithExcludeFields'>\n",
+        )
+        self.assertEqual(PollWithExcludeFields.history.all().count(), 1)
+
 
 class TestCleanOldHistory(TestCase):
     command_name = "clean_old_history"